5b404a2325960eb6f284ac7255030c0c4351b45a
Nabil Abdelouahed <114498779+NabilAbdelouahed@users.noreply.github.com>
2025-12-10 09:19:14 +0000

    fix: sql prepared statements 2

--- check_registration.php
+++ check_registration.php
@@ -3,41 +3,47 @@
 $page_title = "Check registration";
 
 include 'mysqli_connect.php';
-
 include 'includes/header.html';
-
 include 'includes/navbar.html';
 
-if (isset ($_SESSION['username'])){
-	header('location: index.php');
-}
-else{
-	
-    $username = $_POST['username'];
-    $password = $_POST['pass'];
-
-
-    $sql1 = "SELECT users_username, users_password FROM users WHERE users_username = '{$username}' AND users_password = '{$password}'";
-    $sql2 = "INSERT INTO users (users_username, users_password) VALUES ('$username', '$password')";
-
-    $result1 = mysqli_query ($connection, $sql1) or die (mysqli_error ($connection));
-
-    if (mysqli_num_rows ($result1) == 0){
-        if (mysqli_query ($connection, $sql2)){
-            include 'includes/new_registration.php';
-        }
-        else{
-            include 'includes/error.php';
-            
-        }
-
-    }
-    else{
-        include 'includes/notregistered.php';
-    }
+if (isset($_SESSION['username'])) {
+    header('location: index.php');
+    exit();
 }
 
-mysqli_close ($connection);
+$username = $_POST['username'] ?? '';
+$password = $_POST['pass'] ?? '';
+
+// Vérification si l'utilisateur existe (requête préparée)
+$stmt1 = $connection->prepare(
+    "SELECT users_username FROM users WHERE users_username = ? AND users_password = ?"
+);
+$stmt1->bind_param("ss", $username, $password);
+$stmt1->execute();
+$stmt1->store_result();
+
+if ($stmt1->num_rows === 0) {
+
+    // Inscription : requête préparée sécurisée
+    $stmt2 = $connection->prepare(
+        "INSERT INTO users (users_username, users_password) VALUES (?, ?)"
+    );
+    $stmt2->bind_param("ss", $username, $password);
+
+    if ($stmt2->execute()) {
+        include 'includes/new_registration.php';
+    } else {
+        include 'includes/error.php';
+    }
+
+    $stmt2->close();
+
+} else {
+    include 'includes/notregistered.php';
+}
+
+$stmt1->close();
+$connection->close();
 
 include 'includes/footer.html';
 
