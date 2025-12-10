949932e9064b94c8ee412c833b71da2eb6f88b54
Nabil Abdelouahed <114498779+NabilAbdelouahed@users.noreply.github.com>
2025-12-10 14:50:31 +0000

    fix:xss skip html chars

--- login.php
+++ login.php
@@ -7,7 +7,10 @@ include 'includes/header.html';
 include 'includes/navbar.html';
 
 if (isset ($_SESSION['username'])){
-	echo "You are logged! You can " . "<a href='logout.php'>" . "logout" . "</a>";
+
+    echo "You are logged as <strong>"
+        . htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8')
+        . "</strong>. You can <a href='logout.php'>logout</a>";
 }
 else{
     include 'includes/login.html';
--- registration.php
+++ registration.php
@@ -7,7 +7,9 @@ include 'includes/header.html';
 include 'includes/navbar.html';
 
 if (isset ($_SESSION['username'])){
-	echo "Logged in with name '" . $_SESSION['username'] . "'. You can <a href='logout.php'>logout</a>";
+	echo "Logged in with name '<strong>" 
+    . htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8') 
+    . "</strong>'. You can <a href='logout.php'>logout</a>";
 }
 else{
     include 'includes/registration.html';
